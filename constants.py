314982d1ecede9239b1e076ff346ee416fb6059e
Daniel Rösch <droesch91@gmail.com>
2026-07-29 19:57:08 +0200

    create constants file for compiling all magic numbers, colors and other equivalent values into - WIP

---
+++ constants.py
@@ -0,0 +1,18 @@
+RULE_NAMES = {
+    "generate_uid": "UID generieren",
+    "copy_target": "Kopieren aus",
+    "format_date": "Datum (YYYY-MM-DD)",
+    "default_value": "Standardwert",
+    "static_value": "Festwert",
+    "clean_plz": "PLZ (5-stellig)",
+    "gender": "Geschlecht->Anrede",
+    "split_street": "Nur Straße",
+    "split_number": "Nur Hausnummer",
+    "merge_columns": "Spalten zusammenführen",
+    "lookup_ik_provider": "Krankenkasse aus IK",
+    "lookup_plz_by_city": "PLZ aus Ort ergänzen",
+    "lookup_city_by_plz": "Ort aus PLZ ergänzen",
+    "validate_ik": "IK-Nummer prüfen",
+    "validate_kvnr": "Versichertennr. prüfen",
+    "validate_email": "E-Mail prüfen"
+}
--- pygrator.py
+++ pygrator.py
@@ -10,24 +10,6 @@ from db_util import format_date_iso, generate_id, parse_varchar_limit, sanitize_
 from schemas import SCHEMAS
 from dialogs import center_window, ExtraFieldsDialog, RowValidationDialog, ValidationFixDialog, StringCleanupPreviewDialog
 
-RULE_NAMES = {
-    "generate_uid": "UID generieren",
-    "copy_target": "Kopieren aus",
-    "format_date": "Datum (YYYY-MM-DD)",
-    "default_value": "Standardwert",
-    "static_value": "Festwert",
-    "clean_plz": "PLZ (5-stellig)",
-    "gender": "Geschlecht->Anrede",
-    "split_street": "Nur Straße",
-    "split_number": "Nur Hausnummer",
-    "merge_columns": "Spalten zusammenführen",
-    "lookup_ik_provider": "Krankenkasse aus IK",
-    "lookup_plz_by_city": "PLZ aus Ort ergänzen",
-    "lookup_city_by_plz": "Ort aus PLZ ergänzen",
-    "validate_ik": "IK-Nummer prüfen",
-    "validate_kvnr": "Versichertennr. prüfen",
-    "validate_email": "E-Mail prüfen"
-}
 
 # Farbschema & Theme für modernere Optik
 ctk.set_appearance_mode("System")
